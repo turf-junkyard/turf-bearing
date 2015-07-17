aee12dfaefb808d63a31863a4be842ba49a7b493
Frank Rowe <fsrowe@gmail.com>
2015-07-17 14:51:49 -0400

    add option to return compass direction (N, NE, etc)

--- test.js
+++ test.js
@@ -15,3 +15,18 @@ test('bearing', function(t){
   t.ok(bear, 'should return a bearing');
   t.end();
 });
+
+test('bearing --compass', function(t){
+  var pt1 = {
+    "type": "Feature",
+    "geometry": {"type": "Point", "coordinates": [-75.4, 39.4]}
+  };
+  var pt2 = {
+    "type": "Feature",
+    "geometry": {"type": "Point", "coordinates": [-75.534, 39.123]}
+  };
+
+  var bear = bearing(pt1, pt2, 'compass');
+  t.ok(bear, 'should return a bearing');
+  t.end();
+});
